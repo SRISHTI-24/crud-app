f95f4b84cf838498f41c27522f28db082335702b
SRISHTI SAXENA <73647667+SRISHTI-24@users.noreply.github.com>
2023-04-18 02:43:08 +0530

    uodate user first commit

--- index.js
+++ index.js
@@ -55,6 +55,20 @@ app.delete("/deleteuser/:email",(req,res)=>{
         res.json(result)
     })
 })
+
+// update user
+app.put("/updateuser/:email",(req,res)=>{
+    let email = req.params.email
+    const name = req.body.names
+    const phone = req.body.phone
+    const city = req.body.city
+    let sql = `UPDATE employee SET names='${name}', phone='${phone}',city='${city}'`
+    db.query(sql,(err,result)=>{
+        if(err) throw err
+        else 
+        res.json(result)
+    }) 
+})
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log("server is running at ", PORT)
